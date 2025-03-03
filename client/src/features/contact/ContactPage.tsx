bcd63dc9c3186df6926e8e4a121538bfddc2d89f
Yutai Liu <yutailiu@Yutais-MBP.attlocal.net>
2025-03-03 17:31:12 -0500

    Add payload to action in the old way

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
-import { CounterState } from "./counterReducer"
+import { CounterState, decrement, increment } from "./counterReducer"
 import { Button, ButtonGroup, Typography } from "@mui/material"
 
 export default function ContactPage() {
@@ -15,8 +15,9 @@ export default function ContactPage() {
                 The data is: {data}
             </Typography>
             <ButtonGroup>
-                <Button onClick={() => dispatch({type: 'increment'})} color='secondary'>Increment</Button>
-                <Button onClick={() => dispatch({type: 'decrement'})} color='error'>Decrement</Button>
+                <Button onClick={() => dispatch(increment())} color='secondary'>Increment</Button>
+                <Button onClick={() => dispatch(increment(5))} color='primary'>Increment by 5</Button>
+                <Button onClick={() => dispatch(decrement())} color='error'>Decrement</Button>
             </ButtonGroup>
         </div>
     )
--- client/src/features/contact/counterReducer.ts
+++ client/src/features/contact/counterReducer.ts
@@ -6,17 +6,34 @@ const initialState: CounterState = {
     data: 42
 }
 
-export default function counterReducer(state = initialState, action: {type: string}) {
+export function increment(amount = 1) {
+    return {
+        type: 'increment',
+        payload: amount
+    }
+}
+
+export function decrement(amount = 1) {
+    return {
+        type: 'decrement',
+        payload: amount
+    }
+}
+
+export default function counterReducer(state = initialState, 
+    action: { type: string, payload: number }) {
     switch (action.type) {
         case 'increment':
+            // the ... (spread operator) will copy current state and update then replace the current state
+            // prevent mutate/modify state directly is the essential priciple of Redux
             return {
                 ...state,
-                data: state.data + 1
+                data: state.data + action.payload
             }
         case 'decrement':
             return {
                 ...state,
-                data: state.data - 1,
+                data: state.data - action.payload,
             }
         default:
             return state;
